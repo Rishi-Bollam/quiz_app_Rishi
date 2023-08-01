295878362db2984df35184f81e9c0f04e5658347
Rishi-Bollam <136047130+Rishi-Bollam@users.noreply.github.com>
2023-08-01 21:47:17 +0530

    Add files via upload

---
+++ database/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        question: "JavaScript is an ______ language",
+        options: [
+            'Obejct-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 2,
+        question: "Following methods can be used to display data in some form using ",
+        options: [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 3,
+        question: "When an operation value is NULL, the typeof return by the una",
+        options: [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 4,
+        question: "What does the toString method return?",
+        options: [
+            'Return Object',
+            'Return String',
+            'Return Integer',
+        ]
+    },
+    {
+        id: 5,
+        question: "Which function is used to serialize an object into JSON string",
+        options: [
+            'Obejct-Oriented',
+            'parse()',
+            'convert()',
+        ]
+    }
+]
+
+export const answers = [0, 1, 2, 1, 0];
